ebab3d9c2bf69877045b7d8990f6d81eac74c93f
Alan Nato <alanwnato@gmail.com>
2026-07-15 13:08:52 -0500

    Minor Changes

--- aw-server/aw_server/__about__.py
+++ aw-server/aw_server/__about__.py
@@ -16,7 +16,7 @@ projectpath = srcpath.parent
 bundlepath = projectpath.parent  # the ActivityWatch bundle repo, in some circumstances
 
 # This line set by script when run (metaprogramming)
-__version__ = "v0.13.2"
+__version__ = "v0.14.0b1.dev+5f202ce"
 
 
 def get_rev():
